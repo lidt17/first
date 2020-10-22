df313270ee8fb9192d072f799369a8512ec149ae
lidt17 <lidt17@163.com>
2020-10-22 09:40:53 +0800

    Second commit all.   1.Main.java -->update   2.Student.java  -->add

--- src/Main.java
+++ src/Main.java
@@ -2,5 +2,14 @@ public class Main {
 
     public static void main(String[] args) {
         System.out.println("Hello World!"+"Lidt is good!");
+        Student stu = new Student();
+        stu.setId("001");
+        stu.setName("张三");
+        stu.setAge(10);
+
+        System.out.println("id:"+stu.getId());
+        System.out.println("name;"+stu.getName());
+        System.out.println("age:"+stu.getAge());
+
     }
 }
---
+++ src/Student.java
@@ -0,0 +1,35 @@
+/**
+ * User:lidt
+ * Date:2020/10/22
+ * Time:9:33
+ */
+
+public class Student {
+    private String id;
+    private String name;
+    private int age;
+
+    public void setId(String id){
+        this.id = id;
+    }
+
+    public String getId(){
+        return this.id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+}
