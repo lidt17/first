535ba6ece11367f612ab277ae9d8747a8a5dc381
lidt17 <lidt17@163.com>
2020-10-22 15:08:11 +0800

    新增加一行

--- src/Main.java
+++ src/Main.java
@@ -17,7 +17,7 @@ public class Main {
         System.out.println("teacher subject:"+tc.getSubject());
 
         System.out.println("---这是一行代码-   --");
-
+        System.out.println("新增加一行");
 
     }
 }
